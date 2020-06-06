8bdd68c2b602d12141a5e734bd80cb4196e55b32
Misol Goh <melancholy8914@gmail.com>
2020-06-06 23:33:45 +0200

    add background changing by scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Profile from 'app/pages/Profile';
 import Projects from 'app/pages/Projects';
 
 function App() {
+  const [bgIndex, setBgIndex] = useState<number>(1);
+
+  useEffect(() => {
+    const scroll = function () {
+      const rate = Math.round(window.scrollY / window.innerHeight * 100 / 13) + 1;
+
+      setBgIndex((prev) => {
+        return (prev !== rate) ? rate : prev;
+      });
+    };
+
+    window.addEventListener('scroll', scroll);
+
+    return () => {
+      window.removeEventListener('scroll', scroll);
+    }
+  }, []);
+
   return (
     <div className="overflow-auto">
-      <div className="bg-teal-900 text-white">
+      <div className={`transition duration-75 bg-teal-${(10 - bgIndex) * 100} text-gray-${bgIndex * 100}`}>
         <Profile />
-      </div>
-      <div className="bg-teal-100 text-gray-900">
         <Projects />
       </div>
     </div>
