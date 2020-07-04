db8a6aeb80531e9eaab18a691d0c68ff0a1a3312
Misol Goh <melancholy8914@gmail.com>
2020-07-05 01:03:27 +0200

    udpate main's animation

--- src/app/pages/Main/index.tsx
+++ src/app/pages/Main/index.tsx
@@ -6,54 +6,88 @@ import { useScrollForBackground } from 'app/hooks/scroll';
 import { MdCardTravel } from 'react-icons/md';
 import { DiCoffeescript } from 'react-icons/di';
 import { FaBeer } from 'react-icons/fa';
+import { MdComputer } from 'react-icons/md';
+import { FiArrowDown } from 'react-icons/fi';
 
 import 'animate.css';
 
+const favourites = [
+  {
+    text: 'Coffee',
+    Icon: DiCoffeescript,
+  },
+  {
+    text: 'Beer',
+    Icon: FaBeer,
+  },
+  {
+    text: 'Travel',
+    Icon: MdCardTravel,
+  },
+  {
+    text: 'Solving Problems',
+    Icon: MdComputer,
+  },
+];
+
 function Main() {
-  const [animateClassName, setAnimateClassName] = useState<string>(
-    'animate__bounceInRight'
-  );
+  const [flipIndex, setFlipIndex] = useState<number>(-2);
 
   const { divRef } = useScrollForBackground('bg-blue-900');
 
   useEffect(() => {
-    const timeoutFlip = setTimeout(() => {
-      setAnimateClassName('animate__flip');
-    }, 1250);
+    const intervalFlip = setInterval(() => {
+      setFlipIndex((prev) => {
+        if (prev === favourites.length - 1) {
+          clearInterval(intervalFlip);
+          return prev;
+        }
 
-    const timeoutWobble = setTimeout(() => {
-      setAnimateClassName('animate__wobble');
-    }, 2500);
+        return prev + 1;
+      });
+    }, 1500);
 
     return () => {
-      clearTimeout(timeoutFlip);
-      clearTimeout(timeoutWobble);
+      clearInterval(intervalFlip);
     };
   }, []);
+
+  const isLast = (index: number) => index === favourites.length - 1;
 
   return (
     <Container className="h-screen m-auto">
       <div
-        className="relative lg:text-4xl  text-2xl flex h-full md:pl-1/4"
+        className="relative font-sriracha lg:text-4xl  text-2xl flex h-full md:pl-1/4"
         ref={divRef}
       >
-        <div className={`m-auto animate__animated ${animateClassName}`}>
-          <span className="font-sriracha font-bold lg:text-6xl text-5xl">
-            Misol Goh
-          </span>
+        <div className="m-auto animate__animated animate__bounceInRight">
+          <span className="font-bold lg:text-6xl text-5xl">Misol Goh</span>
           <br />
-          <span className="font-sriracha font-semibold lg:text-5xl text-4xl">
-            Front-end Engineer/Developer
+          <span className="text-yellow-500 font-semibold lg:text-5xl text-4xl">
+            Software Developer
           </span>
           <br />
           <span>loves</span>
-          <div className="flex items-center">
-            Travel <MdCardTravel />
-            {` / `}
-            Coffee <DiCoffeescript />
-            {` / `}
-            Beer <FaBeer />
-          </div>
+          {favourites.map(({ text, Icon }, index) => (
+            <div
+              key={text}
+              className={`flex items-center ${
+                index === flipIndex
+                  ? 'block animate__animated animate__flipInX'
+                  : 'hidden'
+              } ${isLast(index) ? 'text-red-300' : ''}`}
+            >
+              <span className="mr-3">{text}</span>
+              <Icon />
+            </div>
+          ))}
+          {isLast(flipIndex) && (
+            <div className="text-2xl flex items-center py-4 animate__animated animate__zoomIn">
+              <span className="mr-3">Wanna know more?</span>
+              <span className="mr-3 font-extrabold">Scroll down</span>
+              <FiArrowDown />
+            </div>
+          )}
         </div>
       </div>
     </Container>
